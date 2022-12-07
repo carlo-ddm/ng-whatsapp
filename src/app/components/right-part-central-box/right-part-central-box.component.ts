721e75407021f0ec40223052346d94deaccd432a
pdcmb <ziomek.mateusz94@gmail.com>
2022-12-07 11:52:21 +0100

    Inserimento dell'orario dell'ultimo messaggio inviato da utente ed intelocutore (in chat)

--- src/app/components/right-part-central-box/right-part-central-box.component.ts
+++ src/app/components/right-part-central-box/right-part-central-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, SimpleChanges } from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
 import { Message, Profile } from 'src/app/interface/profile.interface';
 
 @Component({
@@ -6,21 +6,26 @@ import { Message, Profile } from 'src/app/interface/profile.interface';
   templateUrl: './right-part-central-box.component.html',
   styleUrls: ['./right-part-central-box.component.scss']
 })
-export class RightPartCentralBoxComponent{
+export class RightPartCentralBoxComponent implements OnInit{
+
+  ngOnInit(): void {
+    console.log('data: ',this.data?.messages);
+
+  }
 
   @Input() data:Profile | null | undefined;
 
 
   // ORARIO DI ULTIMO ACCESSO DELL'INTERLOCUTORE *************************
-  // getHours(messages: Message[]): string{
-  //   let messageReceived = messages.filter(messaggio=>messaggio.status === 'received')
-  //   // console.log(messageReceived);
+  getHours(messages: Message[]): string{
+    let messageReceived = messages.filter(messaggio=>messaggio.status === 'sent')
+    // console.log(messageReceived);
 
-  //   if (messageReceived.length > 0) {
-  //     return messageReceived[messageReceived.length - 1]?.date.slice(10)
-  //   } else {
-  //     return ''
-  //   }
-  // }
+    if (messageReceived.length > 0) {
+      return messageReceived[messageReceived.length - 1]?.date.slice(10)
+    } else {
+      return ''
+    }
+  }
 
 }
